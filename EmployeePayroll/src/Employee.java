8791561394ca45ab51fb809206c65f08951fd014
CFSL <CFSL@10.64.159.26>
2018-01-11 10:44:57 -0600

    Initial commit

---
+++ EmployeePayroll/src/Employee.java
@@ -0,0 +1,30 @@
+/* **********************************************************
+ * Programmer:	Rob Sveinson
+ * Class:		CS20S
+ * 
+ * Assignment:	aX  qY
+ *
+ * Description:	describe the class you are creating
+ *
+ * 
+ * *************************************************************
+ */
+ 
+ // import files here as needed
+ 
+ 
+ public class Employee
+ {  // begin class
+ 	
+ 	// *********** class constants **********
+ 	
+ 	
+ 	// ********** instance variable **********
+ 	
+ 	// ********** constructors ***********
+ 	
+ 	// ********** accessors **********
+ 	
+ 	// ********** mutators **********
+ 
+ }  // end class
